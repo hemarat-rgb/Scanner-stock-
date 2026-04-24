16320a4e6291a0a2a3215082d36e6ea02e545f88
hemarat-rgb <hemarat.suk@gmail.com>
2026-04-24 23:05:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,3 +26,17 @@ df = pd.DataFrame(results, columns=["Stock","Price","Score"])
 df = df.sort_values(by="Score", ascending=False)
 
 st.dataframe(df)
+score = 0
+
+ema200 = df["Close"].rolling(200).mean()
+ema50 = df["Close"].rolling(50).mean()
+
+# EMA200
+if len(df) >= 200 and not pd.isna(ema200.iloc[-1]):
+    if last["Close"] > ema200.iloc[-1]:
+        score += 2
+
+# EMA50
+if len(df) >= 50 and not pd.isna(ema50.iloc[-1]):
+    if last["Close"] > ema50.iloc[-1]:
+        score += 1
