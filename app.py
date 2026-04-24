f238d362545bf3a0d3206e7ee07af03a37e20518
hemarat-rgb <hemarat.suk@gmail.com>
2026-04-24 23:12:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 score = 0
 
-ema100 = df["Close"].rolling(100).mean()
 ema50 = df["Close"].rolling(50).mean()
-
-# EMA200
-if len(df) >= 100 and not pd.isna(ema100.iloc[-1]):
-    if last["Close"] > ema100.iloc[-1]:
-        score += 2
 
 # EMA50
 if len(df) >= 50 and not pd.isna(ema50.iloc[-1]):
